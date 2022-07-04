5bfda85feff793636600810999cb8544a12d3c3c
Bongumusa Nala <bongimusa.nala@gmail.com>
2022-07-04 13:06:44 +0200

    third

--- 1/src/test/java/ImpactTest.java
+++ 1/src/test/java/ImpactTest.java
@@ -1,10 +1,10 @@
-import java.util.ArrayList;
 import java.util.stream.IntStream;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.provider.ValueSource;
+
+import junit.framework.Assert;
 
 class ImpactTest {
 ImpactDem impactDem;
@@ -12,6 +12,7 @@ ImpactDem impactDem;
 
 Object WrongResults= IntStream.of(1,3,6,7,8,12,13,14,15,21,22,23,24,31);
 Object[] ExpectedResults = {1, 3, 6-8, 12-15, 21-24, 31};
+Object[] ActualResults = {1,3,-2,-3, -3,31};
 
 	@BeforeEach
 	 void setUp()  {
@@ -19,11 +20,22 @@ Object[] ExpectedResults = {1, 3, 6-8, 12-15, 21-24, 31};
 	}
 
 	@Test
-	
-	void testResults() {
+		void testNotEqualResults() {
 		// Assertions.assertNotNull(impactDem.result);
-		 Assertions.assertEquals(impactDem.result, ExpectedResults);
+		 Assertions.assertEquals(impactDem.result, ActualResults);
 	}
 	
+	@Test
+	void testEqual() {
+		Assertions.assertEquals(impactDem.result, ExpectedResults);
 	
+}
+	
+	@Test
+	void testNull() {
+		Assertions.assertNotNull(impactDem.result);
+	
+}
+	
+
 }
